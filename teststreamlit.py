4af9098b90bc3c8402c5f038eea960ae1bf1aabe
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-07 00:06:05 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+import numpy as np
+number = np.random.randn(10,20)
+st.dataframe(number)
