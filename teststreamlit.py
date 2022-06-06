6a4a3dd302252090591cecf1d82c657e4526818a
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-07 00:02:41 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,4 +1,8 @@
 import streamlit as st
-st.write("Hello world")
-st.write ("Min ka sa yar k=gyi lar")
-st.write("haHa pal ya mal")
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
