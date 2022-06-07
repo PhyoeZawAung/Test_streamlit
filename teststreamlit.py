8adfd2ded88e4c24aae083297ed45f2df21c01d4
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-07 23:23:53 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,12 +1,10 @@
 import streamlit as st
+left,right = st.columns(2)
 
-left_column, right_column = st.columns(2)
-# You can use a column just like st.sidebar:
-left_column.button('Press me!')
-
-# Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
+with left:
+    if st.button("Press me"):
+        "Hello do you press me"
+        
+with right:
+    data = st.radio("Choose the data",'mama','koko','mgmg')
+    print("You choose {data} love")
