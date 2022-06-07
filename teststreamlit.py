0ebce2c481d2516267b10aafd61c4185440507d5
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-07 23:08:24 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -2,3 +2,4 @@ import streamlit as st
 sidebar_selectData  = st.sidebar.selectbox(
 "Choose to contact developar",("mail","Phone:","Facebook")
 )
+sidebar_Slider = at.side.slider("Choose",0.0,100,(25.0,75.0))
