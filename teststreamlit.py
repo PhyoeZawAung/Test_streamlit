3dc668670c433817b75e7b4a271209126683fd7f
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-07 23:05:52 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,14 +1,4 @@
 import streamlit as st
-import pandas as pd
-arr = [1,2,3,4]
-arr1 = [5,6,7,8]
-df = pd.DataFrame({
-    'first column': arr,
-    'second column':arr1
-    })
-df
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
-
-'You selected: ', option
+sidebar_selectData  = st.sidebar.selectbox(
+"Choose to contact developar",("mail","Phone:","Facebook")
+)
