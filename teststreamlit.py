ccfa02c6fea8c777cc512be0f762c4e7e7139b9e
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-07 23:01:29 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
-
+arr = [1,2,3,4]
+arr1 = [5,6,7,8]
 df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
+    'first column': arr,
+    'second column':arr1
     })
 
 option = st.selectbox(
