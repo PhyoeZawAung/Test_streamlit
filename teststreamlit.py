2dc546375ef3cd9d15a0c5eaa53092fe33f66cc0
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-07 22:37:54 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,9 +1,3 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-
-st.dataframe(dataframe.style.highlight_max(axis=0))
+x = st.slider("x")
+st.write(x,"the square is ", x*x)
