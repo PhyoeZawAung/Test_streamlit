dd4699c48c3e46767e32c53fec69d9a45c44f868
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-07 23:30:38 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,10 +1,16 @@
 import streamlit as st
-left,right = st.columns(2)
+import time
 
-with left:
-    if st.button("Press me"):
-        "Hello do you press me"
-        
-with right:
-    data = st.radio("Choose the data",('mama','koko','mgmg'))
-    st.write(f"You choose {data} love")
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
