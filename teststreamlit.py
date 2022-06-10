a16025faade2ac5aad7a1604b055b728960b0616
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-10 23:56:59 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from zipfile import ZipFile
 import json
+from io import StringIO
 messenger_file = st.file_uploader("Enter Your Messenger Dowmloaded file here")
 
 if messenger_file is not None:
   st.write(messenger_file.name)
+  stringio = StringIO(messenger_file.getvalue().decode("utf-8"))
+  st.write(stringio)
   with open(messenger_file.name,"r") as data:
     mdata = json.load(data)
     st.write(mdata)
