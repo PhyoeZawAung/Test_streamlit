64bbce4ba8105f406f902c193458a37b0d76be4d
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-11 00:18:44 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -9,13 +9,14 @@ messenger_file = st.file_uploader("Enter Your Messenger Dowmloaded file here")
 if messenger_file is not None:
   st.write(messenger_file.name)
   jsonData = json.loads(messenger_file.getvalue())
-  st.write(jsonData)
+  
 for message in jsonData["messages"]:
                 try:
                     date = datetime.fromtimestamp(message["timestamp_ms"] / 1000).strftime("%Y-%m-%d %H:%M:%S")
                     sender = message["sender_name"]
                     content = message["content"]
-                    st.write(date , sender , content)
+                    st.write("date     ,sender     ,content")
+                    st.write(date,"     " , sender ,"    ", content)
 
                 except KeyError:
                     pass
