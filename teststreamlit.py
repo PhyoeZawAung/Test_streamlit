163084b0308d9b80b36add2eceaf91a61094ecb8
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-10 23:47:14 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,11 +1,15 @@
 import streamlit as st
 from zipfile import ZipFile
+import json
 messenger_file = st.file_uploader("Enter Your Messenger Dowmloaded file here")
 
 if messenger_file is not None:
   st.write(messenger_file.name)
-  with ZipFile(messenger_file, 'r') as zip:
-    zip.printdir()
+  with open(messenger_file,"r") as data:
+    mdata = json.load(data)
+    st.write(mdata)
+  #with ZipFile(messenger_file, 'r') as zip:
+    #zip.printdir()
     ##st.write('Extracting all the files now...')
     #zip.extractall()
     #st.write('Done!')
