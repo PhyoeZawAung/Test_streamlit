367b5aaf792860abe2a3940727477bfdea26a066
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-11 00:13:25 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -8,7 +8,15 @@ if messenger_file is not None:
   st.write(messenger_file.name)
   jsonData = json.loads(messenger_file.getvalue())
   st.write(jsonData)
-  
+for message in jsonData["messages"]:
+                try:
+                    date = datetime.fromtimestamp(message["timestamp_ms"] / 1000).strftime("%Y-%m-%d %H:%M:%S")
+                    sender = message["sender_name"]
+                    content = message["content"]
+                    st.write(date , sender , content)
+
+                except KeyError:
+                    pass
   
   
   
