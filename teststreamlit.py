f8e7e0d2bc639b35ae5f6fb07cce1017a5ace8da
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-10 17:24:55 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -4,9 +4,10 @@ messenger_file = st.file_uploader("Enter Your Messenger Dowmloaded file here")
 
 if messenger_file is not None:
   st.write(messenger_file.name)
+  with ZipFile(messenger_file, 'r') as zip:
+    st.write(zip.printdir())
 
   
-with ZipFile(messenger_file, 'r') as zip:
-  zip.printdir() 
+
 
      
