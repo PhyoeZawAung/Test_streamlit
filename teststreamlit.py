ed13f6f717b608745c6396835d593ba7e362bb5d
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-10 17:06:58 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -3,12 +3,13 @@ from io import StringIO
 import io
 import pandas as pd
 messenger_file = st.file_uploader("Enter Your Messenger Dowmloaded file here",accept_multiple_files=True)
-dataframe = pd.read_csv(uploaded_file)
-     st.write(dataframe)
+
 if messenger_file is not None:
   for file in messenger_file:
      bytes_data = file.read()
      st.write("filename:",file.name)
+     dataframe = pd.read_csv(file)
+     st.write(dataframe)
      stringio = StringIO(file.getvalue().decode("utf-8"))
      st.write(stringio)
     
