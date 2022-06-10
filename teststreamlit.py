05e725d62a487c60e571facd9c977b15239b1f81
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-10 16:48:24 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 messenger_file = st.file_uploader("Enter Your Messenger Dowmloaded file here",accept_multiple_files=True)
-if messeger_file is not None:
+if messenger_file is not None:
   for file in messenger_file:
      bytes_data = file.read()
      st.write("filename:",file.name)
