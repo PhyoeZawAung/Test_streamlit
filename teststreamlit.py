d45de2a9f5b033c4394bb3bf3235db338bc2a8e9
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-10 14:12:49 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -4,7 +4,7 @@ file = st.file_uploader("Chooose a file")
 if file is not None:
     st.write("file uploaded")
     dataframe = pd.read_csv(file)
-     st.write(dataframe)
+    st.write(dataframe)
     
 
 
