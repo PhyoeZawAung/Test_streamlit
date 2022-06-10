65afb950bec06f65fbd3f785ffcf00514d3f7b65
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-10 17:01:59 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from io import stringIO
+from io import StringIO
 import io
 messenger_file = st.file_uploader("Enter Your Messenger Dowmloaded file here",accept_multiple_files=True)
 if messenger_file is not None:
   for file in messenger_file:
      bytes_data = file.read()
      st.write("filename:",file.name)
-     stringio = io.StringIO(file.getvalue().decode("utf-8"))
+     stringio = StringIO(file.getvalue().decode("utf-8"))
      st.write(stringio)
      
