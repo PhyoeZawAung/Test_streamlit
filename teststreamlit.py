37f4d061388f6ff36d413f253e98ec32978c5bbc
PhyoeZawAung <63353246+PhyoeZawAung@users.noreply.github.com>
2022-06-10 16:36:59 +0630

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1,9 +1,5 @@
 import streamlit as st
-import jina as jn
-st.set_page_config(page_title="Jina Text Search",)
 
-endpoint = "http://0.0.0.0:45678/api/search" # This is Jina's default endpoint. If your Flow uses something different, switch it out
-
-st.title("Jina Text Search")
-
-jn.text_search(endpoint=endpoint)
+messenger_file = st.file_uploader("Enter Your Messenger Dowmloaded file here")
+if messenger_file is not None:
+  st.write("file uploaded")
